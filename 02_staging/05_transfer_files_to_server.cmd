c5379daec66f522c1bf815aa988f853c38df6fa9
Pierre <pierre@pvln.nl>
2018-12-10 09:40:56 +0100

    updates

--- 02_staging/05_transfer_files_to_server.cmd
+++ 02_staging/05_transfer_files_to_server.cmd
@@ -1,4 +1,4 @@
-:: Name:     05_ftp_files_to_server.cmd
+:: Name:     05_staging_files_to_server.cmd
 :: Purpose:  FTP files to downloadserver
 :: Author:   pierre.veelen@pvln.nl
 ::
@@ -47,42 +47,41 @@ cd "%cmd_dir%"
 :: =========================
 ::
 :: remove any existing ..\09_temporary\_ftp_files.txt file
-IF EXIST "..\09_temporary\_ftp_files.txt" (del "..\09_temporary\_ftp_files.txt")
+IF EXIST "..\09_temporary\_staging_files.txt" (del "..\09_temporary\_staging_files.txt")
 ::
-:: Create ..\09_temporary\_ftp_files.txt
-echo %staging_user_downloadserver%>>..\09_temporary\_ftp_files.txt
-echo %staging_pw_downloadserver%>>..\09_temporary\_ftp_files.txt
+:: Create ..\09_temporary\_staging_files.txt
+echo %staging_user_downloadserver%>>..\09_temporary\_staging_files.txt
+echo %staging_pw_downloadserver%>>..\09_temporary\_staging_files.txt
 :: switch to binary mode
-echo binary>>..\09_temporary\_ftp_files.txt
+echo binary>>..\09_temporary\_staging_files.txt
 :: disable prompt; process the mput or mget without requiring any reply
-echo prompt>>..\09_temporary\_ftp_files.txt
+echo prompt>>..\09_temporary\_staging_files.txt
 :: copy files from top level folder
-echo cd %ftp_download_folder%>>..\09_temporary\_ftp_files.txt
-echo mput %output_dir%\*>>..\09_temporary\_ftp_files.txt
+echo cd %ftp_download_folder%>>..\09_temporary\_staging_files.txt
+echo mput %output_dir%\*>>..\09_temporary\_staging_files.txt
 :: copy files from all sub folders
 FOR /f %%G in ('dir /b /A:D "%output_dir%"') DO (
-	echo mkdir %%G>>..\09_temporary\_ftp_files.txt
-    echo cd %%G>>..\09_temporary\_ftp_files.txt
-	echo mput %output_dir%\%%G\*>>..\09_temporary\_ftp_files.txt
-	echo put %output_dir%\index.html>>..\09_temporary\_ftp_files.txt
-	echo cd ..>>..\09_temporary\_ftp_files.txt
+	echo mkdir %%G>>..\09_temporary\_staging_files.txt
+    echo cd %%G>>..\09_temporary\_staging_files.txt
+	echo mput %output_dir%\%%G\*>>..\09_temporary\_staging_files.txt
+	echo put %output_dir%\index.html>>..\09_temporary\_staging_files.txt
+	echo cd ..>>..\09_temporary\_staging_files.txt
     )
-echo bye>>..\09_temporary\_ftp_files.txt
+echo bye>>..\09_temporary\_staging_files.txt
 
 :: run the actual FTP commandfile
-ftp -s:..\09_temporary\_ftp_files.txt %staging_downloadserver%
-del ..\09_temporary\_ftp_files.txt
+ftp -s:..\09_temporary\_staging_files.txt %staging_downloadserver%
+del ..\09_temporary\_staging_files.txt
 
 GOTO CLEAN_EXIT
 
 :ERROR_EXIT
 cd "%cmd_dir%" 
-:: remove any existing _ftp_files.txt file
-IF EXIST "..\09_temporary\_ftp_files.txt" (del "..\09_temporary\_ftp_files.txt")
+:: remove any existing _staging_files.txt file
+IF EXIST "..\09_temporary\_staging_files.txt" (del "..\09_temporary\_staging_files.txt")
 ECHO *******************
 ECHO Error: %ERROR_MESSAGE%
 ECHO *******************
-
    
 :CLEAN_EXIT   
 timeout /T 10
