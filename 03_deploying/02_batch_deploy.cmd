9e7d241c479ad9721367711b53e42d597f69ce5c
Pierre Veelen <pierre@pvln.nl>
2018-12-12 08:24:52 +0100

    Update 02_batch_deploy.cmd

--- 03_deploying/02_batch_deploy.cmd
+++ 03_deploying/02_batch_deploy.cmd
@@ -1,2 +1,15 @@
-::
-CALL 05_ftp_files_to_server pvln
+:: create the required settings
+SET site_name=pvln
+SET extension_name=htaccess
+SET deploy_folder=/joomla01/
+SET secrets_folder=..\..\..\_secrets
+:: run deployment
+CALL 05_deploy_files_to_server.cmd
+
+:: create the required settings
+SET site_name=pvln
+SET extension_name=htaccess
+SET deploy_folder=/joomla01/
+SET secrets_folder=..\..\..\_secrets
+:: run deployment
+CALL 05_deploy_files_to_server.cmd
