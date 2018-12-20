9245afa61da8e11db2f7b3e76f85936b37ca3f75
Pierre <pierre@pvln.nl>
2018-12-20 18:57:57 +0100

    typo's

---
+++ 03_deploying/_get_script_files_pvln.cmd
@@ -0,0 +1,5 @@
+:: retrieve required files from github
+curl https://raw.githubusercontent.com/pierre-pvln/htaccess_creator/master/03_deploying/01_deploy_to_pvln.cmd --output 01_deploy_to_pvln.cmd
+curl https://raw.githubusercontent.com/pierre-pvln/htaccess_creator/master/03_deploying/05_deploy_files.cmd --output 05_deploy_files.cmd
+
+pause
