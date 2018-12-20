c7e48a21461c9f071bd4994a400d8bf0c48bfa73
Pierre <pierre@pvln.nl>
2018-12-20 19:52:26 +0100

    voluntas added

---
+++ 03_deploying/01_deploy_to_voluntas.cmd
@@ -0,0 +1,18 @@
+:: Name:     01_deploy_to_2connect4u.cmd
+:: Purpose:  set enviroment and run deploy script 
+:: Author:   pierre.veelen@pvln.nl
+:: Revision: 2018 12 10 - initial version
+::
+
+@ECHO off
+SETLOCAL ENABLEEXTENSIONS
+
+:: Setting required environment variables:
+::
+SET site_name=voluntas
+SET extension_name=htaccess
+SET deploy_folder=./joomla_01/
+SET secrets_folder=..\..\..\..\_settings
+SET extension_folder=..\..\_5_extensions\_installed\_htaccess
+
+CALL 05_deploy_files.cmd
--- 03_deploying/CHANGELOG.md
+++ 03_deploying/CHANGELOG.md
@@ -6,6 +6,7 @@
 <li>Some files renamed</li>
 <li>Unneeded files removed</li>
 <li>Check if file is available in staging area</li>
+<li>Voluntas added</li>
 </ul>
 
 <h4>v.0.0.1 2018-11-21</h4>
