be561b122a3935ab7a8aeaa6633b2d2e8acff8df
Pierre Veelen <pierre@pvln.nl>
2019-01-27 14:55:28 +0100

    Added cls in script

--- 01_building/02_batch_build.cmd
+++ 01_building/02_batch_build.cmd
@@ -1,7 +1,13 @@
 ::
 @ECHO off
+CLS
 CALL 01_build_file.cmd default
+CLS
 CALL 01_build_file.cmd pvln
+CLS
 CALL 01_build_file.cmd 2connect4u
+CLS
 CALL 01_build_file.cmd ver-bind
+CLS
 CALL 01_build_file.cmd voluntas
+CLS
