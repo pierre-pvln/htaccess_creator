d43fffb40ee770119426b36ac3b68c42308be202
Pierre Veelen <pierre@pvln.nl>
2019-02-16 15:03:13 +0100

    get scripts updated with new file names

--- 03_deploying/_get_script_files_pvln.cmd
+++ 03_deploying/_get_script_files_pvln.cmd
@@ -1,6 +1,6 @@
 :: retrieve required files from github
+curl https://raw.githubusercontent.com/pierre-pvln/htaccess_creator/master/03_deploying/_get_script_files_pvln.cmd --output _get_script_files_pvln.cmd --remote-time
 curl https://raw.githubusercontent.com/pierre-pvln/htaccess_creator/master/03_deploying/01_deploy_to_pvln.cmd --output 01_deploy_to_pvln.cmd --remote-time
 curl https://raw.githubusercontent.com/pierre-pvln/htaccess_creator/master/03_deploying/05_deploy_files.cmd --output 05_deploy_files.cmd --remote-time
-curl https://raw.githubusercontent.com/pierre-pvln/htaccess_creator/master/03_deploying/ftp_get_file.cmd --output ftp_get_file.cmd --remote-time
-curl https://raw.githubusercontent.com/pierre-pvln/htaccess_creator/master/03_deploying/ftp_put_file.cmd --output ftp_put_file.cmd --remote-time
-curl https://raw.githubusercontent.com/pierre-pvln/htaccess_creator/master/03_deploying/_get_script_files_pvln.cmd --output _get_script_files_pvln.cmd --remote-time
+curl https://raw.githubusercontent.com/pierre-pvln/htaccess_creator/master/03_deploying/deploy_ftp_get.cmd --output deploy_ftp_get.cmd --remote-time
+curl https://raw.githubusercontent.com/pierre-pvln/htaccess_creator/master/03_deploying/deploy_ftp_put.cmd --output deploy_ftp_put.cmd --remote-time
