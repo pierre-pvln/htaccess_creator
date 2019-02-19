6b17c553ffa7b4233c8be4e51f80bd3384def57a
Pierre Veelen <pierre@pvln.nl>
2019-02-19 21:57:15 +0100

    Typo's fixed

--- 03_deploying/deploy_ftp_put.cmd
+++ 03_deploying/deploy_ftp_put.cmd
@@ -7,7 +7,7 @@
 :: - deploy_command	    		  command incl path that is used transfer files to staging/download server
 :: - deploy_user_downloadserver   the username on the download server
 :: - deploy_pw_downloadserver     the password for that user
-:: - output_dir                   the folder with files that are transfered (on local machine)
+:: - output_dir    DELETE         the folder with files that are transfered (on local machine)
 :: - temporary_folder             the folder where temporary files are stored (on local machine)
 :: - deploy_downloadserver        the name or ip-address of the staging/download server 
 :: - deploy_folder                the folder where the files are stored in on the staging/download server
@@ -32,10 +32,10 @@ IF "%deploy_pw_downloadserver%" == "" (
    SET ERROR_MESSAGE=[ERROR] [%~n0 ] deploy_pw_downloadserver not set ...
    GOTO ERROR_EXIT_SUBSCRIPT
 )
-IF "%output_dir%" == "" (
-   SET ERROR_MESSAGE=[ERROR] [%~n0 ] output_dir not set ...
-   GOTO ERROR_EXIT_SUBSCRIPT
-)
+::IF "%output_dir%" == "" (
+::   SET ERROR_MESSAGE=[ERROR] [%~n0 ] output_dir not set ...
+::   GOTO ERROR_EXIT_SUBSCRIPT
+::)
 IF "%deploy_downloadserver%" == "" (
    SET ERROR_MESSAGE=[ERROR] [%~n0 ] deploy_downloadserver not set ...
    GOTO ERROR_EXIT_SUBSCRIPT
@@ -55,8 +55,8 @@ IF EXIST "%temporary_folder%\_deploy_files.txt" (del "%temporary_folder%\_deploy
 ::
 :: Create %temporary_folder%\_deploy_files.txt
 ::
-ECHO %deploy_user%>%temporary_folder%\_deploy_files.txt
-ECHO %deploy_pw%>>%temporary_folder%\_deploy_files.txt
+ECHO %deploy_user_downloadserver%>%temporary_folder%\_deploy_files.txt
+ECHO %deploy_pw_downloadserver%>>%temporary_folder%\_deploy_files.txt
 :: switch to binary mode
 ECHO binary>>%temporary_folder%\_deploy_files.txt
 :: disable prompt; process the mput or mget without requiring any reply
@@ -68,7 +68,7 @@ ECHO put .htaccess>>%temporary_folder%\_deploy_files.txt
 ECHO bye>>%temporary_folder%\_deploy_files.txt
 
 :: run the actual FTP commandfile
-"%deploy_command%" -s:%temporary_folder%\_deploy_files.txt %deploy_server%
+"%deploy_command%" -s:%temporary_folder%\_deploy_files.txt %deploy_downloadserver%
 DEL %temporary_folder%\_deploy_files.txt
 GOTO CLEAN_EXIT_SUBSCRIPT
 
