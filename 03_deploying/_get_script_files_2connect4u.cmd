6e97e25b90d2c64771a5a4d4711c81541972ed7c
Pierre Veelen <pierre@pvln.nl>
2019-02-20 22:02:34 +0100

    typo fixed

--- 03_deploying/_get_script_files_2connect4u.cmd
+++ 03_deploying/_get_script_files_2connect4u.cmd
@@ -4,5 +4,5 @@ curl https://raw.githubusercontent.com/pierre-pvln/htaccess_creator/master/03_de
 curl https://raw.githubusercontent.com/pierre-pvln/htaccess_creator/master/03_deploying/05_deploy_files.cmd --output 05_deploy_files.cmd --remote-time
 curl https://raw.githubusercontent.com/pierre-pvln/htaccess_creator/master/03_deploying/deploy_ftp_get.cmd --output deploy_ftp_get.cmd --remote-time
 curl https://raw.githubusercontent.com/pierre-pvln/htaccess_creator/master/03_deploying/deploy_ftp_put.cmd --output deploy_ftp_put.cmd --remote-time
-curl https://raw.githubusercontent.com/pierre-pvln/htaccess_creator/master/03_deploying/deploy_pscp_get.cmd --output deploy_ftp_get.cmd --remote-time
-curl https://raw.githubusercontent.com/pierre-pvln/htaccess_creator/master/03_deploying/deploy_pscp_put.cmd --output deploy_ftp_put.cmd --remote-time
+curl https://raw.githubusercontent.com/pierre-pvln/htaccess_creator/master/03_deploying/deploy_pscp_get.cmd --output deploy_pscp_get.cmd --remote-time
+curl https://raw.githubusercontent.com/pierre-pvln/htaccess_creator/master/03_deploying/deploy_pscp_put.cmd --output deploy_pscp_put.cmd --remote-time
